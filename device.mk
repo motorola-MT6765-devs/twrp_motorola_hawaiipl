677137fefbd02620e5c8b890e077ee865228af60
lostdir69 <lostdirx69@gmail.com>
2023-11-09 09:49:40 +0530

    some changes

--- device.mk
+++ device.mk
@@ -6,6 +6,7 @@
 #
 
 LOCAL_PATH := device/motorola/hawaiipl
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -13,19 +14,23 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
+# Virtual A/B
+ENABLE_VIRTUAL_AB := true
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-mtkimpl.recovery \
     android.hardware.boot@1.0-mtkimpl
 
-# PRODUCT_PACKAGES += \
-#    bootctrl.mt6765
+PRODUCT_PACKAGES += \
+    bootctrl.mt6765
 
-#PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-#    bootctrl.mt6765 \
-#    libgptutils \
-#    libz \
-#    libcutils
+PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    bootctrl.mt6765 \
+    libgptutils \
+    libz \
+    libcutils
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
@@ -37,3 +42,31 @@ PRODUCT_PACKAGES += \
 # MTK PlPath Utils
 PRODUCT_PACKAGES += \
     mtk_plpath_utils.recovery
+
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 31
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+# Additional Libraries
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster4 \
+    libkeymaster41 \
+    libpuresoftkeymasterdevice
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+
+
